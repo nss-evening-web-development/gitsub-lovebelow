8a3eb6d3bb070aeb2ce0944a6811f0ca100f0af8
Whitley Beers <whitleybeers@gmail.com>
2022-11-01 18:53:53 -0500

    added filter function for starred repos

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('Love!')
+
+//IMPORTS
+import { renderToDom } from "./utils/renderToDom";
+import {  } from "module";
+
+//CARD RENDER
+const dataOnDom = (array) => {
+  let domString = "";
+  for (const repo of array) {
+    domString += `<div class="card text-center ${repo.name}" style="width: 12rem;">
+    <div class="card-body">
+      <h5 class="card-title">${repo.name}</h5>
+      <p class="card-text">${repo.description}</p>
+    </div>
+  </div>`;
+  }
+  renderToDom("#studentContainer", domString);
+}
+
+//STARRED REPOS
+function starredRepos(array, typeString) {
+  const repoStars = [];
+  for (const repo of array) {
+    if (repo.starred === true) {
+      repoStars.push(repo);
+    }
+  }
+  return repoStars;
+}
