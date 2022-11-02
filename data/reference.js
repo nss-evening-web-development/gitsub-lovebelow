397c77429d7e99d8ad1f936430289baf96a2636e
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2022-11-01 19:00:11 -0500

    ??

---
+++ data/reference.js
@@ -0,0 +1,99 @@
+export const projectList = [
+	{
+		id: 1,
+		name: "Martin's Aquarium",
+		description: "Pet store specializing in exotic fish.",
+	},
+	{
+		id: 2,
+		name: "Coffee Houses",
+		description: "Comprehensive map of coffee houses in Nashville.",
+	},
+	{
+		id: 3,
+		name: "Backyard Garden App",
+		description: "App to maximize space in small, backyard gardens.",
+	},
+	{
+		id: 4,
+		name: "Liquor License",
+		description: "App to help the at home bartender create delicious libations",
+	},
+];
+
+export const repoList = [
+	{
+		id: 1,
+		name: "repo1",
+		description: "repo1 description",
+		starred: true,
+		pinned: true,
+	},
+	{
+		id: 2,
+		name: "repo2",
+		description: "repo2 description",
+		starred: false,
+		pinned: true,
+	},
+	{
+		id: 3,
+		name: "repo3",
+		description: "repo3 description",
+		starred: true,
+		pinned: false,
+	},
+	{
+		id: 4,
+		name: "repo4",
+		description: "repo4 description",
+		starred: false,
+		pinned: false,
+	},
+	{
+		id: 5,
+		name: "repo5",
+		description: "repo5 description",
+		starred: false,
+		pinned: true,
+	},
+];
+
+export const packages = [
+	{
+		id: 1,
+		name: "Docker",
+		description:
+			"A software platform used for building applications based on containers — small and lightweight execution environments.",
+	},
+	{
+		id: 2,
+		name: "Apache Maven",
+		description:
+			"A default package manager used for the Java programming language and the Java runtime environment.",
+	},
+	{
+		id: 3,
+		name: "NuGet",
+		description:
+			"A free and open source package manager used for the Microsoft development platforms including .NET.",
+	},
+	{
+		id: 4,
+		name: "RubyGems",
+		description:
+			"A standard format for distributing Ruby programs and libraries used for the Ruby programming language.",
+	},
+	{
+		id: 5,
+		name: "npm",
+		description:
+			"A package manager for JavaScript, included with Node.js. npm makes it easy for developers to share and reuse code.",
+	},
+	{
+		id: 6,
+		name: "Containers",
+		description:
+			"A single place for your team to manage Docker images and decide who can see and access your images.",
+	},
+];
