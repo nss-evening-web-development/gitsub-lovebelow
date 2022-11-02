0172cd457529054cc896111d7ea50934f2a11428
Kristen Carroll <carroll.kristen@gmail.com>
2022-11-01 21:36:50 -0500

    project cards rendering

--- components/card.js
+++ components/card.js
@@ -24,3 +24,15 @@ export const packageCard = (Object) => {
 </div>
   `
 }
+
+export const projectCard = (Object) => {
+  return `
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  `
+}
--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('Love!')
+import { projectList } from "./data/reference.js"
+import { renderToDom } from "./utils/renderToDom.js"
+import { projectCard } from "./components/card.js";
+
+const renderProjectCards = (array) => {
+  let refStuff = "<h1 class='text-white'>Projects</h1>";
+
+  array.forEach(item => {
+    refStuff += projectCard(item)
+  })
+
+  renderToDom("#new-project", refStuff)
+};
+
+renderProjectCards(projectList)
+
+
+
+const projectListFormLogic = () => {
+  const projectForm = document.querySelector("project-form")
+
+  projectForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newProject = {
+      project: projectList.length + 1,
+      name: 
+    }
+  })
+};
